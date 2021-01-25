50d31372ee773a1500b60b4e0c11af67a2ee5f93
michele <michele.spinello@spindox.it>
2021-01-25 11:40:04 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import Fields from './components/Fields';
+import Languages from './components/Languages';
+import Translate from './components/Translate';
+
+export default function App () {
+
+    const [language, setLanguage] = useState('ru');
+    const [text, setText] = useState('');
+
+    return (
+        <div>
+            <Fields label="Enter English" onChange={setText} value={text} />
+            <Languages language={language} onLanguageChange={setLanguage}/>
+            <hr/>
+            <Translate text={text} language={language}/>
+        </div>
+    )
+}
+
